760d7480a62a8252cf2a1388cc5094403cd2f90f
Dylan Baker <dylang.baker@googlemail.com>
2019-04-09 14:41:42 +0100

    added run CLI command - not yet implemented

--- lookeraccess/cli.py
+++ lookeraccess/cli.py
@@ -71,10 +71,31 @@ def changes(base_url, client_id, client_secret, uselog, port):
 	log_looker_config_file(conn, looker_config, run_time)
 	click.echo(find_changes(looker_config, new_config))
 
+@click.command()
+@click.argument('base_url', envvar='LOOKER_BASE_URL')
+@click.argument('client_id', envvar='LOOKER_CLIENT_ID')
+@click.argument('client_secret', envvar='LOOKER_CLIENT_SECRET')
+@click.option('--port', default=19999)
+@click.option('--uselog', is_flag=True)
+def run(base_url, client_id, client_secret, uselog, port):
+
+	run_time = datetime.now()
+	conn = LookerConnection(client_id, client_secret, base_url, port)
+	new_config = load_config_files()
+	if uselog:
+		looker_config_raw = load_looker_config_from_logs()
+	else:
+		looker_config_raw = get_looker_config(conn)
+	log_raw_looker_config_file(conn, looker_config_raw, run_time)
+	looker_config = clean_looker_config(looker_config_raw)
+	log_looker_config_file(conn, looker_config, run_time)
+	changes = find_changes(looker_config, new_config)
+
 cli.add_command(connect)
 cli.add_command(pull)
 cli.add_command(validate)
 cli.add_command(changes)
+cli.add_command(run)
 
 if __name__ == '__main__':
 	cli()
