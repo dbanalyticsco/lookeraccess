89456671d919e748236ecd05ad1026b6bc04ee40
Dylan Baker <dylang.baker@googlemail.com>
2019-04-09 14:45:49 +0100

    added install_requires to setup.py - to revisit overly specific version requirements

--- setup.py
+++ setup.py
@@ -8,6 +8,12 @@ setup(
     include_package_data=True,
     install_requires=[
         'Click',
+        'requests==2.21.0',
+        'pyyaml==5.1',
+        'pytest==4.4.0',
+        'coverage==4.5.3',
+        'schema==0.7.0',
+        'setuptools==41.0.0'
     ],
     entry_points='''
         [console_scripts]
